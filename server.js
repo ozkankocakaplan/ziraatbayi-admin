85181c376e76934dc6f0696dc692d90780d47c8d
ozkankocakaplan <ozkankocakaplan07@gmail.com>
2024-11-25 21:41:53 +0300

    Update deployment configuration and enable CORS for API requests

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "build")));
-
+app.use(
+  cors({
+    origin: "http://195.85.207.129:8000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
