a5ddfc1cd93e0a32073f8b5ecb4dcf8408918c93
ozkankocakaplan <ozkankocakaplan07@gmail.com>
2024-11-25 21:59:07 +0300

    Remove CORS package and related middleware from the project

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "build")));
-app.use(
-  cors({
-    origin: "http://195.85.207.129:8000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
